e88b54f2755089bcad8248ff7d5ec93580d37ca8
joetour <joe.tourangeau@gmail.com>
2016-08-11 13:50:10 -0400

    first premier commit :D

---
+++ app/src/main/java/com/disruptiveinno/tower/spamsms/MainActivity.java
@@ -0,0 +1,85 @@
+package com.disruptiveinno.tower.spamsms;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+       final EditText messageSMS = (EditText) findViewById(R.id.message_SMS);
+       final EditText numeroSMS = (EditText) findViewById(R.id.numero_SMS);
+
+        Button btnSendSMS = (Button) findViewById(R.id.sendButton);
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                //
+                sendSMS(numeroSMS.getText().toString(), messageSMS.getText().toString());
+           /*here i can send message to emulator 5556. In Real device
+            *you can change number*/
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void sendSMS(String phoneNumber, String message) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, null, null);
+    }
+}
